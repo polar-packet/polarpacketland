c50f63b71025ae4ee66d1bb702155d67a0d43a53
Darren Ong <darrenong.91@gmail.com>
2024-09-08 02:03:12 +0800

    ts

--- tina/config.ts
+++ tina/config.ts
@@ -46,6 +46,11 @@ export default defineConfig({
             label: "Body",
             isBody: true,
           },
+          {
+            type: 'datetime',
+            name: 'date',
+            label: 'publishDate'
+          },
         ],
       },
     ],

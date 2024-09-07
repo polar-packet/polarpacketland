4b1198fea6d8ce60a7f3e4e8c1000007ea449d10
Darren Ong <darrenong.91@gmail.com>
2024-09-08 01:33:12 +0800

    post test

--- tina/config.ts
+++ tina/config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
       {
         name: "post",
         label: "Posts",
-        path: "content/posts",
+        path: "src/content/posts",
         fields: [
           {
             type: "string",
